1fcea88d84abc4c0e2bd32685ff0c8cc4282db5d
Bruno Almeida <61352086+BrunoVollin@users.noreply.github.com>
2023-02-04 22:45:01 -0300

    init

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from './config/config.service';
 
+
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+
+const corsOptions: CorsOptions = {
+  origin: '*',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+};
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors(corsOptions);
   const config = new ConfigService();
   await app.listen(await config.getPortConfig());
 }
